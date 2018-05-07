1acdadeadb4a8bb7727d8a9fc73e93658aace674
Andi Azry <andiazry@gmail.com>
2018-05-06 20:50:51 -0500

    Initial work on portfolio work hover state

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   $('.overlay__toggle').click(function() {
     $('aside').toggleClass('open');
   });
+
+  $('.overlay__hover').hover(
+    function() {
+      $('.overlay__toggle').css('background-color', '#15624f');
+      $('.overlay__toggle').css('background-image', 'url(img/icons/dribbble.svg)');
+    },
+    function() {
+      $('.overlay__toggle').css('background-color', 'white');
+      $('.overlay__toggle').css('background-image', 'url(img/profilePhoto-min.jpg)');
+    }
+  );
 });
