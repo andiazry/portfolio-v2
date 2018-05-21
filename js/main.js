6ef8dbe8cb495e2b5bf31f7d7a8c2af5e7c41023
Andi Azry <andiazry@gmail.com>
2018-05-20 20:56:48 -0500

    Refactor of main nav and about page

--- js/main.js
+++ js/main.js
@@ -5,40 +5,40 @@ $(document).ready(function() {
 
   $('.containerLeft__projects--list01').hover(
     function() {
-      $('.containerRight__overlay')
-        .removeClass('containerRight__overlay')
+      $('.containerRight__photo')
+        .removeClass('containerRight__photo')
         .addClass('project__overlay--01');
     },
     function() {
       $('.project__overlay--01')
         .removeClass('project__overlay--01')
-        .addClass('containerRight__overlay');
+        .addClass('containerRight__photo');
     }
   );
 
   $('.containerLeft__projects--list02').hover(
     function() {
-      $('.containerRight__overlay')
-        .removeClass('containerRight__overlay')
+      $('.containerRight__photo')
+        .removeClass('containerRight__photo')
         .addClass('project__overlay--02');
     },
     function() {
       $('.project__overlay--02')
         .removeClass('project__overlay--02')
-        .addClass('containerRight__overlay');
+        .addClass('containerRight__photo');
     }
   );
 
   $('.containerLeft__projects--list03').hover(
     function() {
-      $('.containerRight__overlay')
-        .removeClass('containerRight__overlay')
+      $('.containerRight__photo')
+        .removeClass('containerRight__photo')
         .addClass('project__overlay--03');
     },
     function() {
       $('.project__overlay--03')
         .removeClass('project__overlay--03')
-        .addClass('containerRight__overlay');
+        .addClass('containerRight__photo');
     }
   );
 });
